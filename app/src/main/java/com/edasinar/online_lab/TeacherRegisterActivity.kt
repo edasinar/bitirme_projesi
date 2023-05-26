2f4b802bb273d4f8afd895c099796254efc9da1c
edasinar <edasinar15@gmail.com>
2023-05-26 22:34:16 +0300

    eklenen özellik ile gerekli aktivite ve xml dosyaların oluşturulması

---
+++ app/src/main/java/com/edasinar/online_lab/TeacherRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.edasinar.online_lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeacherRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_register)
+    }
+}
