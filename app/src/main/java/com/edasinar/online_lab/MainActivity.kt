9b0f95968310d7a7f14bdbb548510393c8243e8d
edasinar <edasinar15@gmail.com>
2023-06-05 19:15:31 +0300

    udak düzeltmeler

--- app/src/main/java/com/edasinar/online_lab/MainActivity.kt
+++ app/src/main/java/com/edasinar/online_lab/MainActivity.kt
@@ -4,35 +4,16 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.ScrollView
 import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.edasinar.online_lab.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
-import java.util.*
-
-
-/**
- * TODO: burada cardviewlerin isimleri değişecek o kadar o da veritabanından alınacak. pek gerek de olmayabilir duruma göre
- *
- * */
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var auth : FirebaseAuth
-    private lateinit var toggle : ActionBarDrawerToggle
-
-    private lateinit var scrollView: ScrollView
-    private lateinit var actionBarImage: View
-    private lateinit var rectangleAnimation: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
